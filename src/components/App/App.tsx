579e3d310e35fc088d05197a218580e6376c6de6
Alexsandr Kharchenko <googfather8@gmail.com>
2025-08-31 17:47:19 +0300

    Prepare project for Vercel deployment

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from "../VoteOptions/VoteOptions";
-import VoteStats from "../VoteStats/voteStats";
+import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
 import type { Votes, VoteType } from "../../types/votes";
 import css from "./App.module.css";
